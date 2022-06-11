c0b781a73d4bec487fbce0b64aa9570324894a7d
BDedafoe <bmd6260@gmail.com>
2022-06-11 13:11:44 -0500

    final save with 404

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,47 @@
+// Retrieve a list of books from the server
+async function main() {
 
-// Your Code Here
+    let response = await fetch('http://localhost:3001/listBooks')
+    let books = await response.json()
+
+    books.forEach(renderBook)
+}
+
+//Display a list of book title to the admin
+async function renderBook(book) {
+
+    let root = document.querySelector('#root')
+
+    let li = document.createElement('li')
+    li.textContent = book.title
+
+//Give each text input a value: the quantity of the associated book
+
+    let quantityInput = document.createElement('input')
+    quantityInput.value = book.quantity
+
+//Place a submit button next to each text input
+
+    let saveButton = document.createElement('button')
+    saveButton.textContent = 'Save'
+
+saveButton.addEventListener('click', ()=> {
+    fetch('http://localhost:3001/updateBook', {
+
+        header: {
+            "Content-Type": "application/json"
+        },
+        method: 'PATCH',
+        
+        body: JSON.stringify({ id: book.id, quantity: quantityInput.value})
+    })
+
+})
+
+li.append(quantityInput, saveButton)
+
+root.append(li)
+
+}
+
+main()
